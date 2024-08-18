a86ad6b7fcc5a52824a1cd0f9a05fc16a662774d
Nishant4coding <srivastava4nishant@gmail.com>
2024-08-18 09:26:28 +0530

    added team image #4

--- src/components/custom/Team.tsx
+++ src/components/custom/Team.tsx
@@ -1,26 +1,29 @@
-import logo from "../../assets/logo.png";
+// import logo from "../../assets/logo.png";
+import team from "../../assets/team.png";
+
 function Team() {
   return (
-    <section className="w-full max-h-screen flex xl:flex-row flex-col justify-between items-center xl:gap-10">
-      <div className="xl:w-[30%] ">
+    <section className="w-full max-h-screen flex xl:flex-row flex-col justify-between items-center xl:gap-10 gap-5">
+      <div className="xl:w-[30%] gap-5 flex flex-col">
         <h1 className="2xl:text-[9rem] xl:text-[7rem] lg:tet-[6rem] md:text-[5rem] text-[3.2rem] text-center xl:text-left leading-none font-bold">
           Team Behind Our Vision
         </h1>
-        <span className="text-[1rem] text-center font-medium mt-5 ">
+        <span className="text-[1rem] text-center font-medium ">
           Explore the dedicated individuals steering Manparth Bharat. Our team
           combines expertise and commitment to blend ancient knowledge with
           modern education, working towards a unified and enlightened Bharat.
         </span>
       </div>
-      <div className="flex flex-col xl:gap-10 xl:w-[40%] justify-end items-end md:block hidden ">
-        <div className="flex flex-row gap-10 items-end justify-end w-full xl:ml-20">
-          <img src={logo} alt="" className="w-full" />
-          <img src={logo} alt="" className="w-full" />
+      <div className="flex flex-col xl:gap-10 xl:w-[60%] justify-end items-end md:block">
+        <img src={team} alt="" className="w-[40rem] xl:ml-auto" />
+        {/* <div className="flex flex-row gap-10 items-end w-full xl:ml-20">
+          <img src={logo} alt="" className="w-[20rem]" />
+          <img src={logo} alt="" className="w-[20rem]" />
         </div>
         <div className="flex flex-row gap-10  w-full xl:mr-20">
-          <img src={logo} alt="" className="w-full" />
-          <img src={logo} alt="" className="w-full" />
-        </div>
+          <img src={logo} alt="" className="w-[20rem]" />
+          <img src={logo} alt="" className="w-[20rem]" />
+        </div> */}
       </div>
     </section>
   );
